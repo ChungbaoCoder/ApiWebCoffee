aa10a5d75117612e870ce8262b84cfd7c26b3e37
ChungBao <baocnqps40165@gmail.com>
2025-02-02 15:50:02 +0700

    Add simple layout and set up ef db connection

--- CoffeeShop/Program.cs
+++ CoffeeShop/Program.cs
@@ -1,8 +1,17 @@
+using CoffeeShop.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
